084f53d23dbf0bd4e6faa0feb4b2bcf6699d98e7
rmfranken <77491494+rmfranken@users.noreply.github.com>
2023-01-26 11:32:25 +0100

    Improved the dockerfile use command ...rdf_tools\SHACL> docker run -v '/C/users/franken/Desktop:/usr/app/src/validation_reports' python_shacl4 to put the output in a folder of your choice (in my case /C/users etc... )

--- SHACL/pyshacl_shacl.py
+++ SHACL/pyshacl_shacl.py
@@ -5,7 +5,7 @@ date_time = (datetime.now().strftime(r"%Y-%M-%d_%H%M%S"))
 
 data_graph = 'instance_data/invalid.ttl'
 shacl_graph = 'shapes_graphs/ImagingOntology.ttl'
-output_graph = f'validation_reports/output{date_time}.ttl'
+output_graph = f'validation_reports/output{date_time}test.ttl'
 
 result = validate(data_graph = data_graph, shacl_graph = shacl_graph, check_sht_result=False, check_dash_result=False, )
 
