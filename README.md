685b7fe514d0b289fed4852d98e3f627fb3bd7a7
rmfranken <77491494+rmfranken@users.noreply.github.com>
2023-01-26 13:47:59 +0100

    testing performance with patients and sphnOnt

---
+++ README.md
@@ -0,0 +1,4 @@
+This repository has the sole purpose of making it easy to run linked data file conversions and shacl validations with an organized folder structure.
+There is also a docker file which allows you to run the shacl validation in a virtual environment and limit its resource usage.
+
+Still in progress.
--- SHACL/pyshacl_shacl.py
+++ SHACL/pyshacl_shacl.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 date_time = (datetime.now().strftime(r"%Y-%M-%d_%H%M%S"))
 
-data_graph = 'instance_data/invalid.ttl'
-shacl_graph = 'shapes_graphs/ImagingOntology.ttl'
+data_graph = 'instance_data/patients.ttl'
+shacl_graph = 'shapes_graphs/sphnOnt.ttl'
 output_graph = f'validation_reports/output{date_time}test.ttl'
 
 result = validate(data_graph = data_graph, shacl_graph = shacl_graph, check_sht_result=False, check_dash_result=False, )
