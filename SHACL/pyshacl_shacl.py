54b3dab7936a4cf016307c30a51be48a02c9a525
rmfranken <77491494+rmfranken@users.noreply.github.com>
2023-01-30 08:31:24 +0100

    testing performance with patients and sphnOnt

--- SHACL/pyshacl_shacl.py
+++ SHACL/pyshacl_shacl.py
@@ -4,7 +4,7 @@ from datetime import datetime
 date_time = (datetime.now().strftime(r"%Y-%M-%d_%H%M%S"))
 
 data_graph = 'instance_data/patients.ttl'
-shacl_graph = 'shapes_graphs/sphnOnt.ttl'
+shacl_graph = 'shapes_graphs/SHACL-testing.ttl'
 output_graph = f'validation_reports/output{date_time}test.ttl'
 
 result = validate(data_graph = data_graph, shacl_graph = shacl_graph, check_sht_result=False, check_dash_result=False, )
