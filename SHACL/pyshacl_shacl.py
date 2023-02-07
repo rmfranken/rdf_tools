6dd0356923f2fc5b867ffdbeee5a0ca5cb7a1127
supermaxiste <supermegaiperste@hotmail.com>
2023-02-07 11:37:30 +0100

    Added new shapes, modified wrong shapes and validation worked :)

--- SHACL/pyshacl_shacl.py
+++ SHACL/pyshacl_shacl.py
@@ -1,11 +1,12 @@
 from pyshacl import validate
 from datetime import datetime
+import os.path
 
 date_time = (datetime.now().strftime(r"%Y-%M-%d_%H%M%S"))
 
 data_graph = 'SHACL/instance_data/valid.ttl'
 shacl_graph = 'SDSC_Internal_KG/SDSC_IKG_shapes.ttl'
-output_graph = f'validation_reports/output{date_time}test.ttl'
+output_graph = f'SHACL/validation_reports/output{date_time}test.ttl'
 
 result = validate(data_graph = data_graph, shacl_graph = shacl_graph, check_sht_result=False, check_dash_result=False, )
 
