9b85e116820ca55731217e3fb21e248620db23f6
rmfranken <77491494+rmfranken@users.noreply.github.com>
2023-02-08 11:36:26 +0100

    Changed the topbraid_shacl script to use a list of files and time execution

--- SHACL/topbraid_shacl.py
+++ SHACL/topbraid_shacl.py
@@ -1,17 +1,28 @@
 import subprocess
 from time import time
 from datetime import datetime
+import os
 
 #timed validation of instance data based on TopBraid SHACL Engine
 
-date_time = (datetime.now().strftime(r"%Y-%M-%d_%H%M%S"))
+date_time = (datetime.now().strftime(r"%Y-%m-%d_%H%M%S"))
+resultslist = []
+index = 0
+for file in os.scandir(r"./shapes_graphs/test_library/sphnSplit"):
+    index += 1
+    shapesfile = file
 
-datafile = r"instance_data\patients.ttl"
-shapesfile = r"shapes_graphs\sphnOntWOSparql.ttl"
-outputfile = f"validation_reports\output{date_time}.ttl"
+    datafile = r"instance_data/patients.ttl"
+    outputfile = f"validation_reports/output{date_time}-{index}.ttl"
 
-start_time = time()
-subprocess.run([r"C:\Users\franken\Downloads\shacl-1.4.2-bin\shacl-1.4.2\bin\shaclvalidate.bat", "-datafile", datafile, "-shapesfile", shapesfile, "-outputfile", outputfile])
-end_time = time()
+    start_time = time()
+    subprocess.run([r"./shacl-142/bin/shaclvalidate.sh", "-datafile", datafile, "-shapesfile", shapesfile, ">", outputfile ])
+    end_time = time()
+    time_taken = end_time - start_time
 
-print("Time taken: ", end_time - start_time)
+    # print(str(file))
+    resultslist.append(str(file).replace("<DirEntry '","").replace(">","") + " : " + str(time_taken))
+    # print(time_taken)
+
+for r in resultslist:
+    print (r)
