17650eb02f256d0d464983ae7ef5a8545cd63beb
rmfranken <77491494+rmfranken@users.noreply.github.com>
2023-02-21 16:02:47 +0100

    Changed some folder structure, added a config.ini file. Replaced the empty placeholder file with a "empty.git" file. Updated the dockerfile to also copy the config.ini file.

--- SHACL/topbraid_shacl.py
+++ SHACL/topbraid_shacl.py
@@ -2,19 +2,24 @@ import subprocess
 from time import time
 from datetime import datetime
 import os
+import configparser
 
 #timed validation of instance data based on TopBraid SHACL Engine
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-date_time = (datetime.now().strftime(r"%Y-%m-%d_%H%M%S"))
+date_time = (datetime.now().strftime(r"%Y-%m-%d_%H:%M:%S"))
 resultslist = []
 index = 0
-for file in os.scandir(r"./shapes_graphs/test_library/sphnSplit"):
+
+datafile = config['SHACL']['instance_data_path']
+shapes_folder_path = config['SHACL']['shapes_path']
+
+
+for file in os.scandir(shapes_folder_path):
     index += 1
     shapesfile = file
-
-    datafile = r"instance_data/patients.ttl"
-    outputfile = f"validation_reports/output{date_time}-{index}.ttl"
-
+    outputfile = str(config['SHACL']['validation_report_path'] + f"{date_time}-{index}.ttl")
     start_time = time()
     subprocess.run([r"./shacl-142/bin/shaclvalidate.sh", "-datafile", datafile, "-shapesfile", shapesfile, ">", outputfile ])
     end_time = time()
